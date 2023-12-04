112f220ff6f4fc44f3f8574b65f27b86fa1d806a
Damian Boz <damian.boz@ukg.com>
2023-12-04 19:05:43 -0300

    .gitignore is now working 2

--- Flowers.WebAssembly/Services/Contract/IDashboardService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Flowers.DTO;
-
-namespace Flowers.WebAssembly.Services.Contract
-{
-    public interface IDashboardService
-    {
-        Task<ResponseDTO<DashboardDTO>> Summary();
-    }
-}
